31c43306038822d08ecc9195e214dd118299d6fd
Evan <evanmills113@gmail.com>
2020-05-31 16:34:54 -0500

    set up basic structure

--- cloaker.py
+++ cloaker.py
@@ -9,4 +9,17 @@ class Cloaker():
 	def __init__(self):
 		pass
 
-	
+	def detect_object(self):
+		pass
+
+	def filter_object(self):
+		pass
+
+	def get_bounding_box(self):
+		pass
+
+	def find_contours(self):
+		pass
+
+	def capture_background(self):
+		pass
