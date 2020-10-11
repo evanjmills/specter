a874011f35f8ea02f9218ed8f1aed09bcc073e13
Evan Mills <evanmills113@gmail.com>
2020-10-11 10:21:12 -0500

    started work on video processor

--- video_processing/main.py
+++ video_processing/main.py
@@ -1,11 +1,22 @@
 import click
-from cloaker import Cloaker
+import cv2
+# import face_recognition
+# import numpy as np
+# import copy
+# import time
 
+@click.command()
+@click.option('--file', help='Optional video file')
+@click.option('--faces', '-f', multiple=True, help='Images of faces to be removed')
+@click.option('--outfile', '-o', help='File to save the processed video to')
+def main(file, faces, outfile):
+	capture = None
+	if file == None:
+		compile
+	else:
+		capture = cv2.VideoCapture(file)
 
-def main():
-	cloaker = Cloaker()
-
-	click.secho('test', fg='blue')
+	
 
 
 if __name__ == '__main__':
